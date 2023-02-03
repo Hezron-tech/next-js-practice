afb4e08f778eec7818fd20f4e67e2fec369af99b
Hezron-tech <kiprophez@gmail.com>
2023-02-03 12:01:33 +0300

    work on nested routes

---
+++ pages/blog/first.js
@@ -0,0 +1,5 @@
+function FirstBlog(){
+    return <h1>FirstBlog page</h1>
+}
+
+export default  FirstBlog
---
+++ pages/blog/second.js
@@ -0,0 +1,5 @@
+function SecondBlog(){
+    return <h1>SecondBlog page</h1>
+}
+
+export default  SecondBlog
