f66861e55dd47223979c8aa1076efc191716b95e
Hezron-tech <kiprophez@gmail.com>
2023-02-03 12:34:13 +0300

    work on dynamic routing

---
+++ pages/product/index.js
@@ -0,0 +1,12 @@
+function ProductList(){
+    return(
+
+        <>
+        <h1>Product 1</h1>
+        <h2>Product 1</h2>
+        <h3>Product 1</h3>
+        </>
+    )
+}
+
+export default ProductList
